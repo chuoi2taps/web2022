c2c7ede95c6669f3677e766c797c3c52235c6d89
chuoi2taps <thiennnph25438@fpt.edu.vn>
2022-06-03 09:55:53 +0700

    master

---
+++ lab3/src/js/data.js
@@ -0,0 +1,3 @@
+export const productList = [
+    {id:1, name: "Big and Juicy Wagyu Beef Cheeseburger", price: 30}
+]
---
+++ lab3/src/js/main.js
@@ -0,0 +1,4 @@
+import { render } from "./common";
+import { productList } from "./data";
+
+function
